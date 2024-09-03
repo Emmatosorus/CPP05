fba138ba7e835d841e57ed1e920c6e486b96cceb
Emmatosorus <emma@bouteyres.com>
2024-09-03 21:26:49 +0200

    Add✨ : finished ex00, tests missing

--- ex00/includes/Bureaucrat.h
+++ ex00/includes/Bureaucrat.h
@@ -5,8 +5,43 @@
 
 class Bureaucrat
 {
+	public:
+		Bureaucrat( int grade , const std::string name );
+		Bureaucrat( const Bureaucrat & c );
+		~Bureaucrat();
 
+		Bureaucrat & operator=( const Bureaucrat & c );
+
+		const std::string get_name() const;
+		int get_grade() const;
+
+		void	increment();
+		void	decrement();
+
+	private:
+		Bureaucrat();
+		const std::string	name;
+		int					grade;
+		class GradeTooHighException : public std::exception
+		{
+			private:
+				std::string message;
+			public:
+				GradeTooHighException(const char * msg);
+				~GradeTooHighException() _GLIBCXX_TXN_SAFE_DYN _GLIBCXX_NOTHROW;
+				const char * what() const throw();
+		};
+		class GradeTooLowException : public std::exception
+		{
+			private:
+				std::string message;
+			public:
+				GradeTooLowException(const char * msg);
+				~GradeTooLowException() _GLIBCXX_TXN_SAFE_DYN _GLIBCXX_NOTHROW;
+				const char * what() const throw();
+		};
 };
 
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& dt);
 
 #endif
