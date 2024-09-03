36aba24ae2cbde6a1749a970beb332ed420b1b9f
Emmatosorus <emma@bouteyres.com>
2024-09-03 21:26:49 +0200

    Add✨ : finished ex00, tests missing

--- ex00/src/Bureaucrat.cpp
+++ ex00/src/Bureaucrat.cpp
@@ -1,3 +1,82 @@
 #include "../includes/Bureaucrat.h"
 
+Bureaucrat::GradeTooHighException::GradeTooHighException(const char *msg)
+{
+	this->message = msg;
+}
 
+Bureaucrat::GradeTooHighException::~GradeTooHighException() throw()
+{
+
+}
+
+Bureaucrat::GradeTooLowException::GradeTooLowException(const char *msg)
+{
+	this->message = msg;
+}
+
+Bureaucrat::GradeTooLowException::~GradeTooLowException() throw()
+{
+
+}
+
+const char *Bureaucrat::GradeTooHighException::what() const throw()
+{
+	return this->message.c_str();
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return this->message.c_str();
+}
+
+Bureaucrat::Bureaucrat()
+{
+	std::cout << "Bureaucrat : Default constructor called" << std::endl;
+}
+
+Bureaucrat::Bureaucrat( int grade , const std::string name ) : name(name)
+{
+	std::cout << "Bureaucrat : Constructor called with grade : " << grade << std::endl;
+	this->grade = 0;
+	if (grade < 1)
+		throw Bureaucrat::GradeTooHighException("Grade is too HIGH");
+	if (grade > 150)
+		throw Bureaucrat::GradeTooLowException("Grade is too LOW");
+	this->grade = grade;
+}
+
+Bureaucrat::~Bureaucrat()
+{
+	std::cout << "Bureaucrat : Destructor called" << std::endl;
+}
+
+int Bureaucrat::get_grade() const
+{
+	return this->grade;
+}
+
+const std::string Bureaucrat::get_name() const
+{
+	return this->name;
+}
+
+void Bureaucrat::increment()
+{
+	if (this->grade - 1 < 1)
+		throw Bureaucrat::GradeTooHighException("Grade is too HIGH");
+	this->grade--;
+}
+
+void Bureaucrat::decrement()
+{
+	if (this->grade + 1 > 150)
+		throw Bureaucrat::GradeTooLowException("Grade is too LOW");
+	this->grade++;
+}
+
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& dt)
+{
+	os << dt.get_name() << ", bureaucrat grade " << dt.get_grade();
+	return os;
+}
--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -3,5 +3,33 @@
 
 int main(void)
 {
-	std::cout << "lol mdr xD" << std::endl;
+	{
+		try
+		{
+			Bureaucrat steve = Bureaucrat(0, "steve");
+			std::cout << steve << std::endl;
+		}
+		catch (std::exception & e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+		try
+		{
+			Bureaucrat bob = Bureaucrat( 151 , "bob" );
+			std::cout << bob << std::endl;
+		}
+		catch (std::exception & e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+		try
+		{
+			Bureaucrat alex = Bureaucrat(25, "alex");
+			std::cout << alex << std::endl;
+		}
+		catch (std::exception & e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+	}
 }
