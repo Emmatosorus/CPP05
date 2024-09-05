98be5019d2eb232d69b9e07c2a5be67981e70e5c
Emmatosorus <emma@bouteyres.com>
2024-09-05 09:52:25 +0200

    Add ✨: Added some tests in ex01

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -36,13 +36,49 @@ int main(void)
 	std::cout << std::endl << "\x1b[1;36m############### TEST 3 ###############\x1b[0m" << std::endl << std::endl;
 	try
 	{
-		Bureaucrat	steve = Bureaucrat(15, "Bob");
+		Bureaucrat	bob = Bureaucrat(15, "Bob");
 		Form		b = Form( "b21", 2, 10);
 
 		std::cout << std::endl;
 
 		std::cout << b;
-		steve.signForm(b);
+		bob.signForm(b);
+		std::cout << b;
+
+		std::cout << std::endl;
+	}
+	catch (std::exception & e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	std::cout << std::endl << "\x1b[1;36m############### TEST 4 ###############\x1b[0m" << std::endl << std::endl;
+	try
+	{
+		Bureaucrat	alex = Bureaucrat(15, "Alex");
+		Form		b = Form( "b21", 2, 10);
+
+		std::cout << std::endl;
+
+		std::cout << b;
+		b.beSigned(alex);
+		std::cout << b;
+
+		std::cout << std::endl;
+	}
+	catch (std::exception & e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	std::cout << std::endl << "\x1b[1;36m############### TEST 5 ###############\x1b[0m" << std::endl << std::endl;
+	try
+	{
+		Bureaucrat	kerman = Bureaucrat(15, "Kerman");
+		Form		b = Form( "b21", 42, 10);
+
+		std::cout << std::endl;
+
+		std::cout << b;
+		b.beSigned(kerman);
 		std::cout << b;
 
 		std::cout << std::endl;
