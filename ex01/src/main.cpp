121e29b846d05dddc0882e6ad33d947e4831785e
Emmatosorus <emma@bouteyres.com>
2024-09-05 09:30:23 +0200

    Fix 👨‍🔧 : Removed unessasery methods for exeptions

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -7,9 +7,45 @@ int main(void)
 	std::cout << std::endl << "\x1b[1;36m############### TEST 1 ###############\x1b[0m" << std::endl << std::endl;
 	try
 	{
-		Bureaucrat steve = Bureaucrat(15, "steve");
+		Bureaucrat	steve = Bureaucrat(15, "Steve");
 		Form		b = Form( "b42", 42, 10);
 		std::cout << steve << std::endl;
+	}
+	catch (std::exception & e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	std::cout << std::endl << "\x1b[1;36m############### TEST 2 ###############\x1b[0m" << std::endl << std::endl;
+	try
+	{
+		Bureaucrat	steve = Bureaucrat(15, "Steve");
+		Form		b = Form( "b42", 42, 10);
+
+		std::cout << std::endl;
+
+		std::cout << b;
+		steve.signForm(b);
+		std::cout << b;
+
+		std::cout << std::endl;
+	}
+	catch (std::exception & e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	std::cout << std::endl << "\x1b[1;36m############### TEST 3 ###############\x1b[0m" << std::endl << std::endl;
+	try
+	{
+		Bureaucrat	steve = Bureaucrat(15, "Bob");
+		Form		b = Form( "b21", 2, 10);
+
+		std::cout << std::endl;
+
+		std::cout << b;
+		steve.signForm(b);
+		std::cout << b;
+
+		std::cout << std::endl;
 	}
 	catch (std::exception & e)
 	{
